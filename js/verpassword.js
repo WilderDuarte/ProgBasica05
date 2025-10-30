8ff0986439d0a6e944f7212dbaa7cad9cda6c05e
WilderDuarte <waduarten@ufpso.edu.co>
2025-10-29 20:39:07 -0500

    funcionalidad de ver password

--- js/verpassword.js
+++ js/verpassword.js
@@ -6,3 +6,27 @@ toggle_password.addEventListener("click", () => {
     toggle_password.classList.toggle("fa-eye");
     toggle_password.classList.toggle("fa-eye-slash");
 })
+
+function validar(){
+    let password = document.getElementById("password").value;
+    let lowercase = /[a-z]/.test(password);
+    let uppercase = /[A-Z]/.test(password);
+    let number = /\d/.test(password);
+    let specialchar = /[\W_]/.test(password);
+    // alert(password.length);
+    if(password.length < 8){
+        Swal.fire(
+            "Error",
+            "La contraseña debe tener al menos 8 caracteres",
+            "error"
+        );
+        return
+    }else{
+        Swal.fire(
+            "Felicidades",
+            `Su contraseña es correcta: ${password}`,
+            "success"
+        );
+        return
+    }
+}
